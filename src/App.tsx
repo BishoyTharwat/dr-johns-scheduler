9b0dca84e003ae2156fd791009490846914be68c
Bishoy Tharwat <bishoytharwat20@gmail.com>
2026-04-21 20:30:58 +0200

    add basename to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/dr-johns-scheduler">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
